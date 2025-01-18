f3f82355e273017a03332b2d6e0e536e9a5e8e06
haleemairfan <haleemairfan04@gmail.com>
2025-01-19 02:22:49 +0800

    Add backend for song selection

--- Miccdrop/app/(tabs)/_layout.tsx
+++ Miccdrop/app/(tabs)/_layout.tsx
@@ -30,6 +30,7 @@ export default function TabLayout() {
         name="explore"
         options={{
           title: 'Explore',
+          headerShown: false,
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
